683c5bb9ac95299a1e5de04851ae4bf41ec44037
Vishnu2400 <lvishnuvardhan886@gmail.com>
2024-04-23 09:27:16 -0500

    Added layout xml files

--- app/src/main/java/com/example/itmd_555_finalproject/RequestManager.java
+++ app/src/main/java/com/example/itmd_555_finalproject/RequestManager.java
@@ -1,12 +1,15 @@
 package com.example.itmd_555_finalproject;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.example.itmd_555_finalproject.DictionaryModels.DictionaryAPIResponse;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
@@ -21,7 +24,40 @@ public class RequestManager {
             .build();
 
     public RequestManager(Context context) {
+
         this.context = context;
+    }
+
+    public void fetchWordMeaning(onFetchDataListener listener, String word){
+
+          CallAPIDictionaryResp callAPIDictionaryResp = retrofit.create(CallAPIDictionaryResp.class);
+          Call<List<DictionaryAPIResponse>> call = callAPIDictionaryResp.callMeanings(word);
+
+        try{
+            call.enqueue(new Callback<List<DictionaryAPIResponse>>() {
+
+                @Override
+                public void onResponse(Call<List<DictionaryAPIResponse>> call, Response<List<DictionaryAPIResponse>> response) {
+                    if (!response.isSuccessful()){
+                        Toast.makeText(context, "Error!!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    listener.onFetchData(response.body().get(0), response.message());
+
+                }
+
+                @Override
+                public void onFailure(Call<List<DictionaryAPIResponse>> call, Throwable t) {
+                    listener.onError("Failed to Fetch the word!!");
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(context, "An Error Occurred!!!", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
     public interface CallAPIDictionaryResp {
--- app/src/main/java/com/example/itmd_555_finalproject/onFetchDataListener.java
+++ app/src/main/java/com/example/itmd_555_finalproject/onFetchDataListener.java
@@ -1,6 +1,11 @@
 package com.example.itmd_555_finalproject;
 
+import com.example.itmd_555_finalproject.DictionaryModels.DictionaryAPIResponse;
+
 public interface onFetchDataListener {
+
+    void onFetchData(DictionaryAPIResponse apiResponse, String message);
+    void onError(String message);
 
 
 }
